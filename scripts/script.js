c0b045a3af6654465083971cca7c4c4b6ca3f2f4
malika199626 <vfkbrf1996@yandex.ru>
2025-08-11 21:41:52 +0300

    init

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const postsContainer = document.querySelector(".posts-row")
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((res)=>res.json())
+.then((res)=> renderPosts(res))
+
+const renderPosts = (posts) =>{
+    posts.forEach(
+        ({id, userId, title, body})=>
+    (postsContainer.innerHTML += `<div class="col">
+      <div class="card" style="width: 18rem;">
+        <img src="img/2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${title.length>20? title.substring(0,20) + "..." : title}</h5>
+            <p class="card-text">${body.length>100? body.substring(0,100) + "..." : body}</p>
+            <a href="#" class="btn btn-primary">
+            Go somewhere
+            </a>
+          </div>
+      </div>
+    </div>`)
+)
+}
+
